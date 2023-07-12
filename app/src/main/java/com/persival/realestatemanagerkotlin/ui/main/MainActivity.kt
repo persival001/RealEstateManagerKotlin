caa6a0a9ac3af9446b57cae5dc76a62a99e529b7
persival001 <persival@free.fr>
2023-07-12 17:23:52 +0200

    Settings xml in progress

--- app/src/main/java/com/persival/realestatemanagerkotlin/ui/main/MainActivity.kt
+++ app/src/main/java/com/persival/realestatemanagerkotlin/ui/main/MainActivity.kt
@@ -2,14 +2,19 @@ package com.persival.realestatemanagerkotlin.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commitNow
 import com.persival.realestatemanagerkotlin.R
+import com.persival.realestatemanagerkotlin.databinding.ActivityMainBinding
+import com.persival.realestatemanagerkotlin.utils.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val binding by viewBinding { ActivityMainBinding.inflate(it) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
