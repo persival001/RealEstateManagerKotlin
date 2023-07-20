34ae6aeff7c1cfa2d13664063e17f34e83a330f2
persival001 <persival@free.fr>
2023-07-20 16:55:00 +0200

    Location permission in progress

--- app/src/main/java/com/persival/realestatemanagerkotlin/ui/maps/MapFragment.kt
+++ app/src/main/java/com/persival/realestatemanagerkotlin/ui/maps/MapFragment.kt
@@ -2,15 +2,11 @@ package com.persival.realestatemanagerkotlin.ui.maps
 
 import android.graphics.Color
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.GoogleMapOptions
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
@@ -18,33 +14,16 @@ import com.google.android.gms.maps.model.CircleOptions
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.persival.realestatemanagerkotlin.R
-import com.persival.realestatemanagerkotlin.databinding.FragmentMapBinding
-import com.persival.realestatemanagerkotlin.databinding.FragmentPropertiesBinding
-import com.persival.realestatemanagerkotlin.ui.main.MainViewModel
-import com.persival.realestatemanagerkotlin.ui.properties.PropertiesFragment
-import com.persival.realestatemanagerkotlin.ui.properties.PropertiesViewModel
-import com.persival.realestatemanagerkotlin.utils.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
 
     private var googleMap: GoogleMap? = null
-
     private val viewModel by viewModels<MapViewModel>()
 
     companion object {
         fun newInstance() = MapFragment()
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // Create map with Lite Mode enabled
-        val mapOptions = GoogleMapOptions().liteMode(true)
-        val mapFragment = SupportMapFragment.newInstance(mapOptions)
-
-        childFragmentManager.beginTransaction().replace(R.id.map, mapFragment).commit()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
