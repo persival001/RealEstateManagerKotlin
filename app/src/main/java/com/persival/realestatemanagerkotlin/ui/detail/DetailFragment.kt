beeddc883053f83f18fc5c575815996d2d2d22b2
persival001 <persival@free.fr>
2023-07-23 12:53:02 +0200

    Navigate from static map to dynamic map

--- app/src/main/java/com/persival/realestatemanagerkotlin/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/persival/realestatemanagerkotlin/ui/detail/DetailFragment.kt
@@ -1,13 +1,22 @@
 package com.persival.realestatemanagerkotlin.ui.detail
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.persival.realestatemanagerkotlin.BuildConfig.MAPS_API_KEY
 import com.persival.realestatemanagerkotlin.R
 import com.persival.realestatemanagerkotlin.databinding.FragmentDetailBinding
 import com.persival.realestatemanagerkotlin.utils.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import com.bumptech.glide.request.target.Target
+import com.persival.realestatemanagerkotlin.ui.maps.MapFragment
+
 
 @AndroidEntryPoint
 class DetailFragment : Fragment(R.layout.fragment_detail) {
@@ -21,6 +30,27 @@ class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // Show the static map
+        val url =
+            "https://maps.googleapis.com/maps/api/staticmap?center=1600+Amphitheatre+Parkway,+Mountain+View,+CA&zoom=14&size=400x400&markers=color:red%7Clabel:C%7C1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=$MAPS_API_KEY"
+
+        Glide.with(this)
+            .load(url)
+            .override(500, 500)
+            .into(binding.mapImageView)
+
+        // Open the map fragment if clicked
+        binding.mapImageView.setOnClickListener {
+            parentFragmentManager.beginTransaction()
+                .replace(
+                    this.id,
+                    MapFragment.newInstance()
+                )
+                .addToBackStack(null)
+                .commit()
+        }
+
     }
 
 }
